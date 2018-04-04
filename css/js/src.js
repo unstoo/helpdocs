f62c730a8c1b09bbcd51ba772bdfaa4d031b01f3
Yev <unstoo@gmail.com>
2018-04-04 13:12:54 +0300

    Responsive Home.hbs up to 760px

--- css/js/src.js
+++ css/js/src.js
@@ -2,7 +2,7 @@
 
 $(document).ready( () => {
 
-    const HIDDEN_HEDER_CLASSNAME = 'hidden',
+    const HIIDDEN_HEADER_CLASSNAME = 'hidden',
         HIDDEN_HAMBURGERMENU_CLASSNAME = 'hamburger-menu-hidden',
         HIDDEM_HAMBURGERFILLER_CLASSNAME = 'hidden-filler',
         HIDDEN_HAMBURGERSEARCH_CLASSNAME = 'hidden-search',
@@ -19,28 +19,41 @@ $(document).ready( () => {
     hamburgerOpenButton.addEventListener('click', toggleMenus)
     hamburgerCloseButton.addEventListener('click', toggleMenus)
 
-
     function toggleMenus(e) {
-        debugger
-        headerMenu.classList.contains(HIDDEN_HEDER_CLASSNAME) ? 
-            headerMenu.classList.remove(HIDDEN_HEDER_CLASSNAME)
-            : headerMenu.classList.add(HIDDEN_HEDER_CLASSNAME)
         
+        runFunctionIfUiIsReachable(headerMenu, () => {
+            headerMenu.classList.contains(HIIDDEN_HEADER_CLASSNAME) ? 
+                headerMenu.classList.remove(HIIDDEN_HEADER_CLASSNAME)
+                : headerMenu.classList.add(HIIDDEN_HEADER_CLASSNAME)
+        })      
+        runFunctionIfUiIsReachable(hamburgerMenu, () => {
             hamburgerMenu.classList.contains(HIDDEN_HAMBURGERMENU_CLASSNAME) ? 
             hamburgerMenu.classList.remove(HIDDEN_HAMBURGERMENU_CLASSNAME)
             : hamburgerMenu.classList.add(HIDDEN_HAMBURGERMENU_CLASSNAME)
-
+        })
+        runFunctionIfUiIsReachable(hamburgerSearch, () => {
             hamburgerSearch.classList.contains(HIDDEN_HAMBURGERSEARCH_CLASSNAME) ? 
             hamburgerSearch.classList.remove(HIDDEN_HAMBURGERSEARCH_CLASSNAME)
             : hamburgerSearch.classList.add(HIDDEN_HAMBURGERSEARCH_CLASSNAME)
 
+        })
+        runFunctionIfUiIsReachable(hamburgerMenuFiller, () => {
             hamburgerMenuFiller.classList.contains(HIDDEM_HAMBURGERFILLER_CLASSNAME) ? 
             hamburgerMenuFiller.classList.remove(HIDDEM_HAMBURGERFILLER_CLASSNAME)
             : hamburgerMenuFiller.classList.add(HIDDEM_HAMBURGERFILLER_CLASSNAME)
-            
-            /** На Home.hbs приходится отдельно прятать сэрч бар. Тк у него отличная структура :(( */
+        
+        })
+        runFunctionIfUiIsReachable(searchOnHomePage, () => {
+            /** На Home.hbs приходится отдельно прятать сэрч бар. Тк у него загадочная структура :( */
             searchOnHomePage.classList.contains(HIDDEN_HOMESEARCH_CLASSNAME) ? 
             searchOnHomePage.classList.remove(HIDDEN_HOMESEARCH_CLASSNAME)
-            : searchOnHomePage.classList.add(HIDDEN_HOMESEARCH_CLASSNAME)
+            : searchOnHomePage.classList.add(HIDDEN_HOMESEARCH_CLASSNAME)   
+        })     
+    }
+
+    function runFunctionIfUiIsReachable(UI, aFunction) {
+        if (UI) {
+            aFunction()
+        }
     }
 })
